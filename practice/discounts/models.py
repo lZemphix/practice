6b6be122ccccd259248b26be31a830eb7871b07b
Zemphix <vafinaaa51@gmail.com>
2025-05-21 00:35:19 +0500

    Added requirements.txt, Docerfile and changed models

--- practice/discounts/models.py
+++ practice/discounts/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
 
@@ -12,6 +13,7 @@ class Partner(models.Model):
 
     def __str__(self):
         return self.name
+    
     name = models.CharField(max_length=64)
     description = models.TextField()
 
@@ -26,6 +28,6 @@ class Discount(models.Model):
     promocode = models.CharField(max_length=64, null=True, blank=True)
     validity_period = models.CharField(max_length=128)
     partner_description = models.ForeignKey(to=Partner, on_delete=models.PROTECT)
-    image = models.ImageField(upload_to='static/imgs')
+    image = models.ImageField(upload_to='discounts_imgs')
     category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
 
--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from discounts.views import index, discounts
 from django.conf.urls.static import static
-from django.conf.import settings
+from django.conf import settings
 
 
 urlpatterns = [
@@ -27,4 +27,6 @@ urlpatterns = [
     path('', index, name='index'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
